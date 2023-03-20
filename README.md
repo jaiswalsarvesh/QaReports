95df39fd4f7d22a31dc9c16326f46a84d31a33fa
jaiswalsarvesh <115342036+jaiswalsarvesh@users.noreply.github.com>
2023-03-20 17:26:50 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# QaReports
+ 
---
+++ index.js
@@ -0,0 +1,14 @@
+const container=document.getElementById('container-report');
+reportsArray=['esvm.html','esvm.html','esvm.html','esvm.html','esvm.html'];
+console.log(container);
+container.innerHTML='';
+for(let i =0;i<reportsArray.length;i++){
+    let randomHex = Math.floor(Math.random() * 0xffffff).toString(16);
+        randomHex = `#${randomHex.padStart(6, "0")}`;
+        const link = document.createElement("a");
+        link.style=`background-color:${randomHex}`;
+        link.classList.add('item')
+        link.href=reportsArray[i];
+        link.innerText=reportsArray[i].split('.')[0];
+        container.appendChild(link);
+}
